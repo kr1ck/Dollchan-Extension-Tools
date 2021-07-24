19b3ac6da76881afb80e85d8193b4f62b6930898
alexander <apache3dk@gmail.com>
2021-07-24 18:39:22 +0300

    tampermonkey es6 update url

--- Dollchan_Extension_Tools.meta.js
+++ Dollchan_Extension_Tools.meta.js
@@ -5,8 +5,8 @@
 // @author          Sthephan Shinkufag @ FreeDollChan
 // @copyright       © Dollchan Extension Team. See the LICENSE file for license rights and limitations (MIT).
 // @description     Doing some profit for imageboards
-// @icon            https://raw.github.com/SthephanShinkufag/Dollchan-Extension-Tools/master/Icon.png
-// @updateURL       https://raw.github.com/SthephanShinkufag/Dollchan-Extension-Tools/master/Dollchan_Extension_Tools.meta.js
+// @icon            https://raw.github.com/kr1ck/Dollchan-Extension-Tools/master/Icon.png
+// @updateURL       https://raw.github.com/kr1ck/Dollchan-Extension-Tools/master/Dollchan_Extension_Tools.user.js
 // @nocompat        Chrome
 // @run-at          document-start
 // @grant           GM_getValue
